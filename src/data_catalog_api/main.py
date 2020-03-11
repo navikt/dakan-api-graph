903dd7e297e30ab60cdc66c97e626df426fdddcd
andre groseth <andregroseth@hotmail.com>
2020-03-11 13:19:48 +0100

    ADD: Health router endpoint for alive and ready check.

--- src/data_catalog_api/main.py
+++ src/data_catalog_api/main.py
@@ -1,15 +1,8 @@
-from data_catalog_api import store
 from fastapi import FastAPI
-from data_catalog_api.routers import nodes, edges
+from data_catalog_api.routers import nodes, edges, health
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    response = await store.get_count()
-    return response
-
-
 app.include_router(nodes.router)
 app.include_router(edges.router)
+app.include_router(health.router)

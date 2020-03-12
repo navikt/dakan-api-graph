e4505ebebad9537390a9fd29a5f574f4926c7fe8
JeremiahUy <jery_3000@hotmail.com>
2020-03-12 10:39:43 +0100

    UPDATE: mapping of static folder when mounting

--- src/data_catalog_api/main.py
+++ src/data_catalog_api/main.py
@@ -4,7 +4,7 @@ from data_catalog_api.routers import nodes, edges, health
 
 app = FastAPI()
 
-app.mount("/src/data_catalog_api/static", StaticFiles(directory="static"), name="static")
+app.mount("src/data_catalog_api/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(nodes.router)
 app.include_router(edges.router)

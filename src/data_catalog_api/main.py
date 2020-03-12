8c11725315c25b02978e7252e8cab1a500b16355
JeremiahUy <jery_3000@hotmail.com>
2020-03-12 10:58:32 +0100

    UPDATE: disabled default docs page for fastapi and add new docs page loading method

--- src/data_catalog_api/main.py
+++ src/data_catalog_api/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from data_catalog_api.routers import nodes, edges, health
+from fastapi.openapi.docs import get_swagger_ui_html
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
 app.mount("/static", StaticFiles(directory="src/data_catalog_api/static"), name="static")
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
 
 app.include_router(nodes.router)
 app.include_router(edges.router)

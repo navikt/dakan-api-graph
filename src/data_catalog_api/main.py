2c607e0c2568ec331fc753db0323fba3a433693a
JeremiahUy <jery_3000@hotmail.com>
2020-03-12 10:17:38 +0100

    ADD: static files to support fastapi docs website

--- src/data_catalog_api/main.py
+++ src/data_catalog_api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from data_catalog_api.routers import nodes, edges, health
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(nodes.router)
 app.include_router(edges.router)

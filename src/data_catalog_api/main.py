9bd78a09491c008046ba4b7fd9e2b68306831c74
JeremiahUy <jery_3000@hotmail.com>
2020-03-19 08:55:20 +0100

    TEST: using starlette-exporter with prometheus client and fixed favicon 404 error

--- src/data_catalog_api/main.py
+++ src/data_catalog_api/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from data_catalog_api.routers import nodes, edges, health, metrics
+from data_catalog_api.routers import nodes, edges, health
 from fastapi.openapi.docs import get_swagger_ui_html
+from starlette_exporter import PrometheusMiddleware, handle_metrics
 
-app = FastAPI(docs_url=None, redoc_url=None)
+app = FastAPI(docs_url=None, redoc_url=None, swagger_static={"favicon": "/static/favicon.png"})
 
 app.mount("/static", StaticFiles(directory="src/data_catalog_api/static"), name="static")
 
@@ -17,7 +18,8 @@ async def custom_swagger_ui_html():
         swagger_css_url="/static/swagger-ui.css",
     )
 
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", handle_metrics)
 app.include_router(nodes.router)
 app.include_router(edges.router)
 app.include_router(health.router)
-app.include_router(metrics.router)

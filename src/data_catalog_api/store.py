d6970e54ca0e41739c4266d55826496183890baf
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-05-05 15:41:30 +0200

    update query

--- src/data_catalog_api/store.py
+++ src/data_catalog_api/store.py
@@ -99,7 +99,7 @@ async def upsert_node(nodes: List[Node]):
             params = f"{params}.property('{key}','{value}')"
 
         query += f".V().has('label','{node.label}').has('id','{node.id}').fold().coalesce(unfold(){params}," \
-            f"addV('{node.label}').property('id','{node.id}').property('name','test'){params})"
+            f"addV('{node.label}').property('id','{node.id}').property('version','1'){params})"
     try:
         print(query)
         res = submit(query)
